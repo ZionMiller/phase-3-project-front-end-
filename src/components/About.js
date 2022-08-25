ab7bd22df8d7bde75fe374d8cf88b61aefe92e92
Zion Miller <djzionmiller@gmail.com>
2022-08-25 15:24:21 -0700

    added icons to dev on about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Image, Grid} from 'semantic-ui-react'
+import { Card, Image, Grid, Icon} from 'semantic-ui-react'
 
 function About() {
   return (
@@ -8,17 +8,46 @@ function About() {
       </Image.Group>
       <Grid centered columns={3}>
         <br />
-      </Grid>
+      </Grid >
         <h1 id="meet-developers">Our Developers:</h1>
         <Card.Group itemsPerRow={3}>
         <Card>
           <Image src='https://www.ctvnews.ca/polopoly_fs/1.2842838.1459624452!/httpImage/image.jpg_gen/derivatives/landscape_960/image.jpg' wrapped ui={false} />
-          <Card.Content>
+          <Card.Content textAlign='center'>
             <Card.Header>Garret Galant</Card.Header>
             <Card.Meta>Full Stack Software Engineer</Card.Meta>
             <Card.Description>
               Garret Galant is a fullstack enggineer from Colorado.
             </Card.Description>
+            <Icon name="github"/>
+            <Icon name="linkedin"/>
+            <Icon name="medium"/>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image src='https://www.ctvnews.ca/polopoly_fs/1.2842838.1459624452!/httpImage/image.jpg_gen/derivatives/landscape_960/image.jpg' wrapped ui={false} />
+          <Card.Content textAlign='center'>
+            <Card.Header>Zion Miller</Card.Header>
+            <Card.Meta>Full Stack Software Engineer</Card.Meta>
+            <Card.Description>
+              Zion Miller is a fullstack enggineer from California.
+            </Card.Description>
+            <Icon name="github"/>
+            <Icon name="linkedin"/>
+            <Icon name="medium"/>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image src='https://www.ctvnews.ca/polopoly_fs/1.2842838.1459624452!/httpImage/image.jpg_gen/derivatives/landscape_960/image.jpg' wrapped ui={false} />
+          <Card.Content textAlign='center'>
+            <Card.Header>Tommy Nguyen</Card.Header>
+            <Card.Meta>Full Stack Software Engineer</Card.Meta>
+            <Card.Description>
+              Tommy Nguyen is a fullstack enggineer from California.
+            </Card.Description>
+            <Icon name="github"/>
+            <Icon name="linkedin"/>
+            <Icon name="medium"/>
           </Card.Content>
         </Card>
         </Card.Group>
